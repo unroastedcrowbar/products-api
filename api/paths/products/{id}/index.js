29a3a8c763ea559250de4b7906990c6ecd799bcf
Dan <dan@Dans-MacBook-Pro.local>
2022-09-12 02:56:50 -0400

    Connect to proper db

--- api/paths/products/{id}/index.js
+++ api/paths/products/{id}/index.js
@@ -1,23 +1,24 @@
 const fetch = require('node-fetch');
 const mongoose = require('mongoose');
 
+// Connect to the DB.
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/products-api');
-const productSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
+mongoose.connect('mongodb+srv://product-pricing-client:pw1234@cluster0.x1k7sdk.mongodb.net/test');
+const db = mongoose.connection;
+const productPriceSchema = new mongoose.Schema({
+  productId: Number,
+  value: Number,
+  currency_code: String,
 });
+const Product = mongoose.model('Product', productPriceSchema);
 
-
+// Get aggregate product data.
+const getPricing = async (productId) => 
+  await Product.find({productId});
 const getProduct = productId => 
   fetch(
     `https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys&tcin=${productId}`,
   );
-
-const getPricingByProductId = productId => ({
-    value: 10.49,
-    currency_code: 'USD',
-  });
 
 
 module.exports = function () {
@@ -28,22 +29,34 @@ module.exports = function () {
       const productResponse = await getProduct(productId);
       const productData = await productResponse.json();
 
-      const pricingData = getPricingByProductId(productId);
+      const pricingData = await getPricing(productId);
+      // This DB happens to have duplicate id's so use only the first.
+      const {value, currency_code} = pricingData[0];
 
       const product = {
         id: productData.data.product.tcin,
         name: productData.data.product.item.product_description.title,
         current_price: {
-          pricingData,
+          value,
+          currency_code,
         },
       };
 
       res.status(200).json(product);
     }
+
+    async function PUT(req, res, next) {
+      new Product({
+        productId: req.params.id,
+        value: req.params.current_price.value,
+        currency_code: req.params.current_price.currency_code,
+      }).save();
+      res.status(200).send();
+    }
   
     GET.apiDoc = {
-      summary: "Fetch aggregate data for a product.",
-      operationId: "getProduct",
+      summary: 'Fetch aggregate data for a product.',
+      operationId: 'getProduct',
       parameters: [
         {
           in: 'path',
@@ -55,11 +68,11 @@ module.exports = function () {
       ],
       responses: {
         200: {
-          description: "Aggregate data for a product.",
+          description: 'Aggregate data for a product.',
           schema: {
-            type: "object",
+            type: 'object',
             items: {
-              $ref: "#/definitions/Product",
+              $ref: '#/definitions/Product',
             },
             example: {
               id: 13860428,
@@ -70,6 +83,31 @@ module.exports = function () {
               },
             },
           },
+        },
+      },
+    };
+
+    PUT.apiDoc = {
+      summary: 'Update the product\'s price.',
+      operationId: 'updateProductPrice',
+      parameters: [
+        {
+          in: 'path',
+          name: 'id',
+          required: true,
+          type: 'number',
+        },
+        {
+          in: 'body',
+          name: 'product',
+          schema: {
+            $ref: "#/definitions/Product",
+          },
+        },
+      ],
+      responses: {
+        200: {
+          description: 'Updated ok',
         },
       },
     };
