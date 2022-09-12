195f95fbbcec9ed866a0c3a49eaccdda4dcbee29
unroastedcrowbar <8432353+unroastedcrowbar@users.noreply.github.com>
2022-09-12 00:23:22 -0400

    Add comment for api endpoint

--- api/paths/products/{id}/index.js
+++ api/paths/products/{id}/index.js
@@ -6,6 +6,8 @@ module.exports = function () {
       const productId = req.params.id;
       const product = { id: productId, name: "Peanut butter" };
 
+      // TODO: Return data from
+      // https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys&tcin=13860428
       res.status(200).json(product);
     }
   
