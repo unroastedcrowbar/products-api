4490a2ed774b773e00f9535beec6ef4fcdf082ba
Dan <dan@Dans-MacBook-Pro.local>
2022-09-12 01:48:45 -0400

    var to const

--- api/paths/products/{id}/index.js
+++ api/paths/products/{id}/index.js
@@ -1,21 +1,41 @@
-var fetch = require('node-fetch');
+const fetch = require('node-fetch');
+const mongoose = require('mongoose');
 
-const getProductById = id => 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/products-api');
+const productSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+});
+
+
+const getProduct = productId => 
   fetch(
-    `https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys&tcin=${id}`,
+    `https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys&tcin=${productId}`,
   );
+
+const getPricingByProductId = productId => ({
+    value: 10.49,
+    currency_code: 'USD',
+  });
+
 
 module.exports = function () {
     let operations = {GET};
   
     async function GET(req, res, next) {
-      const response = await getProductById(req.params.id);
-      const data = await response.json();
-      console.log(data);
+      const productId = req.params.id;
+      const productResponse = await getProduct(productId);
+      const productData = await productResponse.json();
+
+      const pricingData = getPricingByProductId(productId);
 
       const product = {
-        id: data.data.product.tcin,
-        name: data.data.product.item.product_description.title,
+        id: productData.data.product.tcin,
+        name: productData.data.product.item.product_description.title,
+        current_price: {
+          pricingData,
+        },
       };
 
       res.status(200).json(product);
@@ -44,7 +64,11 @@ module.exports = function () {
             example: {
               id: 13860428,
               name: 'The Big Lebowski (Blu-ray) (Widescreen)',
-            }
+              current_price: {
+                value: 13.49,
+                currency_code: 'USD',
+              },
+            },
           },
         },
       },
